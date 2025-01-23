f479639bdb66c375ebd00b4225be4dc4212cfc3e
AllenGlines23 <147121978+AllenGlines23@users.noreply.github.com>
2025-01-23 12:45:13 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TaskProvider } from "./TaskContext"; // Import TaskProvider for global state management
 import Dashboard from "./Dashboard";
 import Tasks from "./Tasks";
-import Navbar from "./Navbar"; // Optional, if Navbar is used for navigation
+import Navbar from "./Navbar"; // Import Navbar component
 import Home from "./Home"; // Import Home component
 
 const App = () => (
   <TaskProvider>
     <Router>
-      <Navbar /> {/* Optional navigation bar */}
+      <Navbar /> {/* Navigation bar */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tasks" element={<Tasks />} />
       </Routes>
     </Router>
